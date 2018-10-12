3c24b74699de445a86d9d29109f000f4ece7ce62
Rene <rene.evertz.98@gmail.com>
2018-10-12 23:12:26 +0200

    Added attributes and propeties to Booking

--- Carsharing/Carsharing/Booking.cs
+++ Carsharing/Carsharing/Booking.cs
@@ -8,5 +8,51 @@ namespace Carsharing
 {
 	class Booking
 	{
+		#region Attributes
+		private Customer customer;
+		private Vehicle vehicle;
+		private DateTime startTimePoint;
+		private DateTime endTimePoint;
+		private double startMileage;
+		private double endMileague;
+		#endregion
+
+		#region Propeties
+		public Customer Customer
+		{
+			get { return customer; }
+			set { customer = value; }
+		}
+
+		public Vehicle Vehicle
+		{
+			get { return vehicle; }
+			set { vehicle = value; }
+		}
+
+		public DateTime StartTimePoint
+		{
+			get { return startTimePoint; }
+			set { startTimePoint = value; }
+		}
+
+		public DateTime EndTimePoint
+		{
+			get { return endTimePoint; }
+			set { endTimePoint = value; }
+		}
+
+		public double StartMilieage
+		{
+			get { return startMileage; }
+			set { startMileage = value; }
+		}
+
+		public double EndMileague
+		{
+			get { return endMileague; }
+			set { endMileague = value; }
+		}
+		#endregion
 	}
 }
