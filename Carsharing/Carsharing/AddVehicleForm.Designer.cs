e36e4ea901523aabd36eaa31021b1cf302f4255c
Rene <rene.evertz.98@gmail.com>
2018-10-15 17:59:03 +0200

    Added Units to VehicleAddForm

--- Carsharing/Carsharing/AddVehicleForm.Designer.cs
+++ Carsharing/Carsharing/AddVehicleForm.Designer.cs
@@ -62,6 +62,13 @@
 			this.maskedTextBasicPrice = new System.Windows.Forms.MaskedTextBox();
 			this.maskedTextPricePerKilometre = new System.Windows.Forms.MaskedTextBox();
 			this.maskedTextPricePerMinute = new System.Windows.Forms.MaskedTextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// labelNumberPlate
@@ -364,11 +371,81 @@
 			this.maskedTextPricePerMinute.TabIndex = 60;
 			this.maskedTextPricePerMinute.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
 			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(245, 104);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(21, 13);
+			this.label1.TabIndex = 61;
+			this.label1.Text = "km";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(245, 156);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(9, 13);
+			this.label2.TabIndex = 62;
+			this.label2.Text = "l";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(245, 286);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(24, 13);
+			this.label3.TabIndex = 63;
+			this.label3.Text = "kW";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(245, 364);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(9, 13);
+			this.label4.TabIndex = 64;
+			this.label4.Text = "l";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(245, 390);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(13, 13);
+			this.label5.TabIndex = 65;
+			this.label5.Text = "€";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(245, 416);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(38, 13);
+			this.label6.TabIndex = 66;
+			this.label6.Text = "€ / km";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(245, 442);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(40, 13);
+			this.label7.TabIndex = 67;
+			this.label7.Text = "€ / min";
+			// 
 			// AddVehicleForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(584, 561);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
 			this.Controls.Add(this.maskedTextPricePerMinute);
 			this.Controls.Add(this.maskedTextPricePerKilometre);
 			this.Controls.Add(this.maskedTextBasicPrice);
@@ -447,5 +524,12 @@
 		private System.Windows.Forms.MaskedTextBox maskedTextBasicPrice;
 		private System.Windows.Forms.MaskedTextBox maskedTextPricePerKilometre;
 		private System.Windows.Forms.MaskedTextBox maskedTextPricePerMinute;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
 	}
 }
