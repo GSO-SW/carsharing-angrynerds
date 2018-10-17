82c2147220d2cb4f2ae46a5010317301eccf6bc1
Jurek <jurvoel@gmx.de>
2018-10-17 20:35:19 +0200

    changed == string.Empty to string.IsNullOrWhiteSpace

--- Carsharing/Carsharing/UserRegistrationForm.cs
+++ Carsharing/Carsharing/UserRegistrationForm.cs
@@ -19,22 +19,22 @@ namespace Carsharing
 
 		private void acceptButton_Click(object sender, EventArgs e)
 		{
-			if (nameTextBox.Text == string.Empty)
+			if (string.IsNullOrWhiteSpace(nameTextBox.Text))
 			{
 				MessageBox.Show("Bitte geben Sie ihren Vornamen an.", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
 				return;
 			}
-			if (lastNameTextBox.Text == string.Empty)
+			if (string.IsNullOrWhiteSpace(lastNameTextBox.Text))
 			{
 				MessageBox.Show("Bitte geben Sie ihren Nachnamen an.", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
 				return;
 			}
-			if (emailTextBox.Text == string.Empty || !emailTextBox.Text.Contains("@") || !emailTextBox.Text.Contains("."))
+			if (string.IsNullOrWhiteSpace(emailTextBox.Text) || !emailTextBox.Text.Contains("@") || !emailTextBox.Text.Contains("."))
 			{
 				MessageBox.Show("Bitte geben Sie eine gültige E-Mail-Adresse an.", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
 				return;
 			}
-			if (phoneTextBox.Text == string.Empty || !phoneTextBox.Text.Any(char.IsDigit)) //nur nummern?
+			if (string.IsNullOrWhiteSpace(phoneTextBox.Text == ) || !phoneTextBox.Text.Any(char.IsDigit)) //nur nummern?
 			{
 				MessageBox.Show("Bitte geben Sie eine gültige Telefonnummer an.", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
 				return;
@@ -49,27 +49,27 @@ namespace Carsharing
 				MessageBox.Show("Bitte wählen Sie ein Passwort mit mindestens 8 Zeichen.", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
 				return;
 			}
-			if (streetTextBox.Text == string.Empty)
+			if (string.IsNullOrWhiteSpace(streetTextBox.Text))
 			{
 				MessageBox.Show("Bitte geben Sie ihre Straße an.", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
 				return;
 			}
-			if (houseNumberTextBox.Text == string.Empty)
+			if (string.IsNullOrWhiteSpace(houseNumberTextBox.Text))
 			{
 				MessageBox.Show("Bitte geben Sie ihre Hausnummer an.", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
 				return;
 			}
-			if (PLZTextBox.Text == string.Empty || !PLZTextBox.Text.All(char.IsDigit))
+			if (string.IsNullOrWhiteSpace(PLZTextBox.Text) || !PLZTextBox.Text.All(char.IsDigit))
 			{
 				MessageBox.Show("Bitte geben Sie eine gültige Postleitzahl an.", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
 				return;
 			}
-			if (cityTextBox.Text == string.Empty)
+			if (string.IsNullOrWhiteSpace(cityTextBox.Text))
 			{
 				MessageBox.Show("Bitte geben Sie ihren Wohnort an.", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
 				return;
 			}
-			if (countryTextBox.Text == string.Empty)
+			if (string.IsNullOrWhiteSpace(countryTextBox.Text))
 			{
 				MessageBox.Show("Bitte geben Sie ihr Heimatland an.", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
 				return;
