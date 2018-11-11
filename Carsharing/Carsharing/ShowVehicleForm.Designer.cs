bff3ce5dcdf077dd2609cc64a8e3f340fe9fcc85
Jurek <jurvoel@gmx.de>
2018-11-11 14:32:36 +0100

    added more display information

--- Carsharing/Carsharing/ShowVehicleForm.Designer.cs
+++ Carsharing/Carsharing/ShowVehicleForm.Designer.cs
@@ -54,6 +54,44 @@
 			this.tankFillingLabel = new System.Windows.Forms.Label();
 			this.coordLabel = new System.Windows.Forms.Label();
 			this.bookingButton = new System.Windows.Forms.Button();
+			this.label13 = new System.Windows.Forms.Label();
+			this.label14 = new System.Windows.Forms.Label();
+			this.label15 = new System.Windows.Forms.Label();
+			this.label16 = new System.Windows.Forms.Label();
+			this.label17 = new System.Windows.Forms.Label();
+			this.label18 = new System.Windows.Forms.Label();
+			this.label19 = new System.Windows.Forms.Label();
+			this.label20 = new System.Windows.Forms.Label();
+			this.label21 = new System.Windows.Forms.Label();
+			this.label28 = new System.Windows.Forms.Label();
+			this.label29 = new System.Windows.Forms.Label();
+			this.label30 = new System.Windows.Forms.Label();
+			this.fuelConsupLabel = new System.Windows.Forms.Label();
+			this.numberOfSeatsLabel = new System.Windows.Forms.Label();
+			this.fuelLabel = new System.Windows.Forms.Label();
+			this.klimaLabel = new System.Windows.Forms.Label();
+			this.tempoLabel = new System.Windows.Forms.Label();
+			this.radioLabel = new System.Windows.Forms.Label();
+			this.bluetoothLabel = new System.Windows.Forms.Label();
+			this.usbLabel = new System.Windows.Forms.Label();
+			this.cdLabel = new System.Windows.Forms.Label();
+			this.basepriceLabel = new System.Windows.Forms.Label();
+			this.pricePerkmLabel = new System.Windows.Forms.Label();
+			this.pricePerMinLabel = new System.Windows.Forms.Label();
+			this.smokerLabel = new System.Windows.Forms.Label();
+			this.winterLabel = new System.Windows.Forms.Label();
+			this.seatHeatingLabel = new System.Windows.Forms.Label();
+			this.espLabel = new System.Windows.Forms.Label();
+			this.absLabel = new System.Windows.Forms.Label();
+			this.naviLabel = new System.Windows.Forms.Label();
+			this.label27 = new System.Windows.Forms.Label();
+			this.label26 = new System.Windows.Forms.Label();
+			this.label25 = new System.Windows.Forms.Label();
+			this.label24 = new System.Windows.Forms.Label();
+			this.label23 = new System.Windows.Forms.Label();
+			this.label22 = new System.Windows.Forms.Label();
+			this.label31 = new System.Windows.Forms.Label();
+			this.availableLabel = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// vehicleListBox
@@ -284,18 +322,398 @@
 			// bookingButton
 			// 
 			this.bookingButton.Enabled = false;
-			this.bookingButton.Location = new System.Drawing.Point(397, 396);
+			this.bookingButton.Location = new System.Drawing.Point(323, 386);
 			this.bookingButton.Name = "bookingButton";
-			this.bookingButton.Size = new System.Drawing.Size(75, 23);
+			this.bookingButton.Size = new System.Drawing.Size(289, 23);
 			this.bookingButton.TabIndex = 27;
 			this.bookingButton.Text = "Buchen";
 			this.bookingButton.UseVisualStyleBackColor = true;
+			// 
+			// label13
+			// 
+			this.label13.AutoSize = true;
+			this.label13.Location = new System.Drawing.Point(506, 15);
+			this.label13.Name = "label13";
+			this.label13.Size = new System.Drawing.Size(86, 13);
+			this.label13.TabIndex = 28;
+			this.label13.Text = "Anzahl der Sitze:";
+			// 
+			// label14
+			// 
+			this.label14.AutoSize = true;
+			this.label14.Location = new System.Drawing.Point(506, 41);
+			this.label14.Name = "label14";
+			this.label14.Size = new System.Drawing.Size(52, 13);
+			this.label14.TabIndex = 29;
+			this.label14.Text = "Kraftstoff:";
+			// 
+			// label15
+			// 
+			this.label15.AutoSize = true;
+			this.label15.Location = new System.Drawing.Point(506, 67);
+			this.label15.Name = "label15";
+			this.label15.Size = new System.Drawing.Size(100, 13);
+			this.label15.TabIndex = 30;
+			this.label15.Text = "Kraftstoffverbrauch:";
+			// 
+			// label16
+			// 
+			this.label16.AutoSize = true;
+			this.label16.Location = new System.Drawing.Point(506, 93);
+			this.label16.Name = "label16";
+			this.label16.Size = new System.Drawing.Size(67, 13);
+			this.label16.TabIndex = 31;
+			this.label16.Text = "Klimaanlage:";
+			// 
+			// label17
+			// 
+			this.label17.AutoSize = true;
+			this.label17.Location = new System.Drawing.Point(506, 119);
+			this.label17.Name = "label17";
+			this.label17.Size = new System.Drawing.Size(60, 13);
+			this.label17.TabIndex = 32;
+			this.label17.Text = "Tempomat:";
+			// 
+			// label18
+			// 
+			this.label18.AutoSize = true;
+			this.label18.Location = new System.Drawing.Point(506, 145);
+			this.label18.Name = "label18";
+			this.label18.Size = new System.Drawing.Size(38, 13);
+			this.label18.TabIndex = 33;
+			this.label18.Text = "Radio:";
+			// 
+			// label19
+			// 
+			this.label19.AutoSize = true;
+			this.label19.Location = new System.Drawing.Point(506, 171);
+			this.label19.Name = "label19";
+			this.label19.Size = new System.Drawing.Size(55, 13);
+			this.label19.TabIndex = 34;
+			this.label19.Text = "Bluetooth:";
+			// 
+			// label20
+			// 
+			this.label20.AutoSize = true;
+			this.label20.Location = new System.Drawing.Point(506, 197);
+			this.label20.Name = "label20";
+			this.label20.Size = new System.Drawing.Size(32, 13);
+			this.label20.TabIndex = 35;
+			this.label20.Text = "USB:";
+			// 
+			// label21
+			// 
+			this.label21.AutoSize = true;
+			this.label21.Location = new System.Drawing.Point(506, 223);
+			this.label21.Name = "label21";
+			this.label21.Size = new System.Drawing.Size(60, 13);
+			this.label21.TabIndex = 36;
+			this.label21.Text = "CD-Spieler:";
+			// 
+			// label28
+			// 
+			this.label28.AutoSize = true;
+			this.label28.Location = new System.Drawing.Point(506, 249);
+			this.label28.Name = "label28";
+			this.label28.Size = new System.Drawing.Size(61, 13);
+			this.label28.TabIndex = 37;
+			this.label28.Text = "Grundpreis:";
+			// 
+			// label29
+			// 
+			this.label29.AutoSize = true;
+			this.label29.Location = new System.Drawing.Point(506, 275);
+			this.label29.Name = "label29";
+			this.label29.Size = new System.Drawing.Size(58, 13);
+			this.label29.TabIndex = 38;
+			this.label29.Text = "Preis / km:";
+			// 
+			// label30
+			// 
+			this.label30.AutoSize = true;
+			this.label30.Location = new System.Drawing.Point(506, 301);
+			this.label30.Name = "label30";
+			this.label30.Size = new System.Drawing.Size(60, 13);
+			this.label30.TabIndex = 39;
+			this.label30.Text = "Preis / min:";
+			// 
+			// fuelConsupLabel
+			// 
+			this.fuelConsupLabel.AutoSize = true;
+			this.fuelConsupLabel.Location = new System.Drawing.Point(612, 67);
+			this.fuelConsupLabel.Name = "fuelConsupLabel";
+			this.fuelConsupLabel.Size = new System.Drawing.Size(10, 13);
+			this.fuelConsupLabel.TabIndex = 40;
+			this.fuelConsupLabel.Text = "-";
+			// 
+			// numberOfSeatsLabel
+			// 
+			this.numberOfSeatsLabel.AutoSize = true;
+			this.numberOfSeatsLabel.Location = new System.Drawing.Point(612, 15);
+			this.numberOfSeatsLabel.Name = "numberOfSeatsLabel";
+			this.numberOfSeatsLabel.Size = new System.Drawing.Size(10, 13);
+			this.numberOfSeatsLabel.TabIndex = 41;
+			this.numberOfSeatsLabel.Text = "-";
+			// 
+			// fuelLabel
+			// 
+			this.fuelLabel.AutoSize = true;
+			this.fuelLabel.Location = new System.Drawing.Point(612, 41);
+			this.fuelLabel.Name = "fuelLabel";
+			this.fuelLabel.Size = new System.Drawing.Size(10, 13);
+			this.fuelLabel.TabIndex = 42;
+			this.fuelLabel.Text = "-";
+			// 
+			// klimaLabel
+			// 
+			this.klimaLabel.AutoSize = true;
+			this.klimaLabel.Location = new System.Drawing.Point(612, 93);
+			this.klimaLabel.Name = "klimaLabel";
+			this.klimaLabel.Size = new System.Drawing.Size(10, 13);
+			this.klimaLabel.TabIndex = 43;
+			this.klimaLabel.Text = "-";
+			// 
+			// tempoLabel
+			// 
+			this.tempoLabel.AutoSize = true;
+			this.tempoLabel.Location = new System.Drawing.Point(612, 119);
+			this.tempoLabel.Name = "tempoLabel";
+			this.tempoLabel.Size = new System.Drawing.Size(10, 13);
+			this.tempoLabel.TabIndex = 44;
+			this.tempoLabel.Text = "-";
+			// 
+			// radioLabel
+			// 
+			this.radioLabel.AutoSize = true;
+			this.radioLabel.Location = new System.Drawing.Point(612, 145);
+			this.radioLabel.Name = "radioLabel";
+			this.radioLabel.Size = new System.Drawing.Size(10, 13);
+			this.radioLabel.TabIndex = 45;
+			this.radioLabel.Text = "-";
+			// 
+			// bluetoothLabel
+			// 
+			this.bluetoothLabel.AutoSize = true;
+			this.bluetoothLabel.Location = new System.Drawing.Point(612, 171);
+			this.bluetoothLabel.Name = "bluetoothLabel";
+			this.bluetoothLabel.Size = new System.Drawing.Size(10, 13);
+			this.bluetoothLabel.TabIndex = 46;
+			this.bluetoothLabel.Text = "-";
+			// 
+			// usbLabel
+			// 
+			this.usbLabel.AutoSize = true;
+			this.usbLabel.Location = new System.Drawing.Point(612, 197);
+			this.usbLabel.Name = "usbLabel";
+			this.usbLabel.Size = new System.Drawing.Size(10, 13);
+			this.usbLabel.TabIndex = 47;
+			this.usbLabel.Text = "-";
+			// 
+			// cdLabel
+			// 
+			this.cdLabel.AutoSize = true;
+			this.cdLabel.Location = new System.Drawing.Point(612, 223);
+			this.cdLabel.Name = "cdLabel";
+			this.cdLabel.Size = new System.Drawing.Size(10, 13);
+			this.cdLabel.TabIndex = 48;
+			this.cdLabel.Text = "-";
+			// 
+			// basepriceLabel
+			// 
+			this.basepriceLabel.AutoSize = true;
+			this.basepriceLabel.Location = new System.Drawing.Point(612, 249);
+			this.basepriceLabel.Name = "basepriceLabel";
+			this.basepriceLabel.Size = new System.Drawing.Size(10, 13);
+			this.basepriceLabel.TabIndex = 49;
+			this.basepriceLabel.Text = "-";
+			// 
+			// pricePerkmLabel
+			// 
+			this.pricePerkmLabel.AutoSize = true;
+			this.pricePerkmLabel.Location = new System.Drawing.Point(612, 275);
+			this.pricePerkmLabel.Name = "pricePerkmLabel";
+			this.pricePerkmLabel.Size = new System.Drawing.Size(10, 13);
+			this.pricePerkmLabel.TabIndex = 50;
+			this.pricePerkmLabel.Text = "-";
+			// 
+			// pricePerMinLabel
+			// 
+			this.pricePerMinLabel.AutoSize = true;
+			this.pricePerMinLabel.Location = new System.Drawing.Point(612, 301);
+			this.pricePerMinLabel.Name = "pricePerMinLabel";
+			this.pricePerMinLabel.Size = new System.Drawing.Size(10, 13);
+			this.pricePerMinLabel.TabIndex = 51;
+			this.pricePerMinLabel.Text = "-";
+			// 
+			// smokerLabel
+			// 
+			this.smokerLabel.AutoSize = true;
+			this.smokerLabel.Location = new System.Drawing.Point(778, 223);
+			this.smokerLabel.Name = "smokerLabel";
+			this.smokerLabel.Size = new System.Drawing.Size(10, 13);
+			this.smokerLabel.TabIndex = 63;
+			this.smokerLabel.Text = "-";
+			// 
+			// winterLabel
+			// 
+			this.winterLabel.AutoSize = true;
+			this.winterLabel.Location = new System.Drawing.Point(778, 197);
+			this.winterLabel.Name = "winterLabel";
+			this.winterLabel.Size = new System.Drawing.Size(10, 13);
+			this.winterLabel.TabIndex = 62;
+			this.winterLabel.Text = "-";
+			// 
+			// seatHeatingLabel
+			// 
+			this.seatHeatingLabel.AutoSize = true;
+			this.seatHeatingLabel.Location = new System.Drawing.Point(778, 171);
+			this.seatHeatingLabel.Name = "seatHeatingLabel";
+			this.seatHeatingLabel.Size = new System.Drawing.Size(10, 13);
+			this.seatHeatingLabel.TabIndex = 61;
+			this.seatHeatingLabel.Text = "-";
+			// 
+			// espLabel
+			// 
+			this.espLabel.AutoSize = true;
+			this.espLabel.Location = new System.Drawing.Point(778, 145);
+			this.espLabel.Name = "espLabel";
+			this.espLabel.Size = new System.Drawing.Size(10, 13);
+			this.espLabel.TabIndex = 60;
+			this.espLabel.Text = "-";
+			// 
+			// absLabel
+			// 
+			this.absLabel.AutoSize = true;
+			this.absLabel.Location = new System.Drawing.Point(778, 119);
+			this.absLabel.Name = "absLabel";
+			this.absLabel.Size = new System.Drawing.Size(10, 13);
+			this.absLabel.TabIndex = 59;
+			this.absLabel.Text = "-";
+			// 
+			// naviLabel
+			// 
+			this.naviLabel.AutoSize = true;
+			this.naviLabel.Location = new System.Drawing.Point(778, 93);
+			this.naviLabel.Name = "naviLabel";
+			this.naviLabel.Size = new System.Drawing.Size(10, 13);
+			this.naviLabel.TabIndex = 58;
+			this.naviLabel.Text = "-";
+			// 
+			// label27
+			// 
+			this.label27.AutoSize = true;
+			this.label27.Location = new System.Drawing.Point(672, 223);
+			this.label27.Name = "label27";
+			this.label27.Size = new System.Drawing.Size(92, 13);
+			this.label27.TabIndex = 57;
+			this.label27.Text = "Raucherfahrzeug:";
+			// 
+			// label26
+			// 
+			this.label26.AutoSize = true;
+			this.label26.Location = new System.Drawing.Point(672, 197);
+			this.label26.Name = "label26";
+			this.label26.Size = new System.Drawing.Size(67, 13);
+			this.label26.TabIndex = 56;
+			this.label26.Text = "Winterreifen:";
+			// 
+			// label25
+			// 
+			this.label25.AutoSize = true;
+			this.label25.Location = new System.Drawing.Point(672, 171);
+			this.label25.Name = "label25";
+			this.label25.Size = new System.Drawing.Size(67, 13);
+			this.label25.TabIndex = 55;
+			this.label25.Text = "Sitzheitzung:";
+			// 
+			// label24
+			// 
+			this.label24.AutoSize = true;
+			this.label24.Location = new System.Drawing.Point(672, 145);
+			this.label24.Name = "label24";
+			this.label24.Size = new System.Drawing.Size(31, 13);
+			this.label24.TabIndex = 54;
+			this.label24.Text = "ESP:";
+			// 
+			// label23
+			// 
+			this.label23.AutoSize = true;
+			this.label23.Location = new System.Drawing.Point(672, 119);
+			this.label23.Name = "label23";
+			this.label23.Size = new System.Drawing.Size(31, 13);
+			this.label23.TabIndex = 53;
+			this.label23.Text = "ABS:";
+			// 
+			// label22
+			// 
+			this.label22.AutoSize = true;
+			this.label22.Location = new System.Drawing.Point(672, 93);
+			this.label22.Name = "label22";
+			this.label22.Size = new System.Drawing.Size(90, 13);
+			this.label22.TabIndex = 52;
+			this.label22.Text = "Navigationsgerät:";
+			// 
+			// label31
+			// 
+			this.label31.AutoSize = true;
+			this.label31.Location = new System.Drawing.Point(506, 327);
+			this.label31.Name = "label31";
+			this.label31.Size = new System.Drawing.Size(90, 13);
+			this.label31.TabIndex = 64;
+			this.label31.Text = "Zurzeit verfügbar:";
+			// 
+			// availableLabel
+			// 
+			this.availableLabel.AutoSize = true;
+			this.availableLabel.Location = new System.Drawing.Point(612, 327);
+			this.availableLabel.Name = "availableLabel";
+			this.availableLabel.Size = new System.Drawing.Size(10, 13);
+			this.availableLabel.TabIndex = 65;
+			this.availableLabel.Text = "-";
 			// 
 			// ShowVehicleForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.availableLabel);
+			this.Controls.Add(this.label31);
+			this.Controls.Add(this.smokerLabel);
+			this.Controls.Add(this.winterLabel);
+			this.Controls.Add(this.seatHeatingLabel);
+			this.Controls.Add(this.espLabel);
+			this.Controls.Add(this.absLabel);
+			this.Controls.Add(this.naviLabel);
+			this.Controls.Add(this.label27);
+			this.Controls.Add(this.label26);
+			this.Controls.Add(this.label25);
+			this.Controls.Add(this.label24);
+			this.Controls.Add(this.label23);
+			this.Controls.Add(this.label22);
+			this.Controls.Add(this.pricePerMinLabel);
+			this.Controls.Add(this.pricePerkmLabel);
+			this.Controls.Add(this.basepriceLabel);
+			this.Controls.Add(this.cdLabel);
+			this.Controls.Add(this.usbLabel);
+			this.Controls.Add(this.bluetoothLabel);
+			this.Controls.Add(this.radioLabel);
+			this.Controls.Add(this.tempoLabel);
+			this.Controls.Add(this.klimaLabel);
+			this.Controls.Add(this.fuelLabel);
+			this.Controls.Add(this.numberOfSeatsLabel);
+			this.Controls.Add(this.fuelConsupLabel);
+			this.Controls.Add(this.label30);
+			this.Controls.Add(this.label29);
+			this.Controls.Add(this.label28);
+			this.Controls.Add(this.label21);
+			this.Controls.Add(this.label20);
+			this.Controls.Add(this.label19);
+			this.Controls.Add(this.label18);
+			this.Controls.Add(this.label17);
+			this.Controls.Add(this.label16);
+			this.Controls.Add(this.label15);
+			this.Controls.Add(this.label14);
+			this.Controls.Add(this.label13);
 			this.Controls.Add(this.bookingButton);
 			this.Controls.Add(this.coordLabel);
 			this.Controls.Add(this.tankFillingLabel);
@@ -357,5 +775,43 @@
 		private System.Windows.Forms.Label tankFillingLabel;
 		private System.Windows.Forms.Label coordLabel;
 		private System.Windows.Forms.Button bookingButton;
+		private System.Windows.Forms.Label label13;
+		private System.Windows.Forms.Label label14;
+		private System.Windows.Forms.Label label15;
+		private System.Windows.Forms.Label label16;
+		private System.Windows.Forms.Label label17;
+		private System.Windows.Forms.Label label18;
+		private System.Windows.Forms.Label label19;
+		private System.Windows.Forms.Label label20;
+		private System.Windows.Forms.Label label21;
+		private System.Windows.Forms.Label label28;
+		private System.Windows.Forms.Label label29;
+		private System.Windows.Forms.Label label30;
+		private System.Windows.Forms.Label fuelConsupLabel;
+		private System.Windows.Forms.Label numberOfSeatsLabel;
+		private System.Windows.Forms.Label fuelLabel;
+		private System.Windows.Forms.Label klimaLabel;
+		private System.Windows.Forms.Label tempoLabel;
+		private System.Windows.Forms.Label radioLabel;
+		private System.Windows.Forms.Label bluetoothLabel;
+		private System.Windows.Forms.Label usbLabel;
+		private System.Windows.Forms.Label cdLabel;
+		private System.Windows.Forms.Label basepriceLabel;
+		private System.Windows.Forms.Label pricePerkmLabel;
+		private System.Windows.Forms.Label pricePerMinLabel;
+		private System.Windows.Forms.Label smokerLabel;
+		private System.Windows.Forms.Label winterLabel;
+		private System.Windows.Forms.Label seatHeatingLabel;
+		private System.Windows.Forms.Label espLabel;
+		private System.Windows.Forms.Label absLabel;
+		private System.Windows.Forms.Label naviLabel;
+		private System.Windows.Forms.Label label27;
+		private System.Windows.Forms.Label label26;
+		private System.Windows.Forms.Label label25;
+		private System.Windows.Forms.Label label24;
+		private System.Windows.Forms.Label label23;
+		private System.Windows.Forms.Label label22;
+		private System.Windows.Forms.Label label31;
+		private System.Windows.Forms.Label availableLabel;
 	}
 }
